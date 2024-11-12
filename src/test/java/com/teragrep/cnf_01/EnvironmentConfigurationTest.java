f8e54981a0ab014d43afdcb60c2c944ac757a86b
51-code <146736881+51-code@users.noreply.github.com>
2024-11-12 11:57:23 +0200

    Support environment variables as a configuration source (#21)
    * Support environment variables as a configuration source
    
    Refactor EnvironmentConfiguration
    
    Support environment variables as a configuration source
    
    Enforce a deep copied map in default configurations
    
    * Add logging to EnvironmentConfiguration

---
+++ src/test/java/com/teragrep/cnf_01/EnvironmentConfigurationTest.java
@@ -0,0 +1,147 @@
+/*
+ * Teragrep Configuration Library for Java (cnf_01)
+ * Copyright (C) 2024 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.cnf_01;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnvironmentConfigurationTest {
+
+    @Test
+    public void testEnvironment() {
+        EnvironmentConfiguration env = new EnvironmentConfiguration();
+        Map<String, String> map = env.asMap();
+
+        // Can't test environment variables reliably, just testing if the map has values.
+        Assertions.assertFalse(map.isEmpty());
+    }
+
+    @Test
+    public void testMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("foo", "bar");
+
+        EnvironmentConfiguration env = new EnvironmentConfiguration(map);
+        // modifying the original doesn't have effect on the result
+        map.put("bar", "foo");
+
+        Map<String, String> result = env.asMap();
+
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertTrue(result.containsKey("foo"));
+        Assertions.assertEquals("bar", result.get("foo"));
+    }
+
+    @Test
+    public void testEmptyMap() {
+        Map<String, String> map = new HashMap<>();
+        EnvironmentConfiguration env = new EnvironmentConfiguration(map);
+        // modifying the original doesn't have effect on the result
+        map.put("foo", "bar");
+
+        Map<String, String> result = env.asMap();
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testEqualsEnvironment() {
+        EnvironmentConfiguration env1 = new EnvironmentConfiguration();
+        EnvironmentConfiguration env2 = new EnvironmentConfiguration();
+
+        env1.asMap();
+
+        Assertions.assertEquals(env1, env2);
+    }
+
+    @Test
+    public void testEqualsMap() {
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("foo", "bar");
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("foo", "bar");
+
+        EnvironmentConfiguration env1 = new EnvironmentConfiguration(map1);
+        EnvironmentConfiguration env2 = new EnvironmentConfiguration(map2);
+
+        env1.asMap();
+
+        Assertions.assertEquals(env1, env2);
+    }
+
+    @Test
+    public void testNotEquals() {
+        Map<String, String> map = new HashMap<>();
+        map.put("foo", "bar");
+        EnvironmentConfiguration env1 = new EnvironmentConfiguration();
+        EnvironmentConfiguration env2 = new EnvironmentConfiguration(map);
+
+        Assertions.assertNotEquals(env1, env2);
+    }
+
+    @Test
+    public void testHashCode() {
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("foo", "bar");
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("foo", "bar");
+        EnvironmentConfiguration env1 = new EnvironmentConfiguration(map1);
+        EnvironmentConfiguration env2 = new EnvironmentConfiguration(map2);
+        EnvironmentConfiguration difEnv = new EnvironmentConfiguration();
+
+        Assertions.assertEquals(env1.hashCode(), env2.hashCode());
+        Assertions.assertNotEquals(env1.hashCode(), difEnv.hashCode());
+    }
+
+    @Test
+    public void testEqualsVerifier() {
+        EqualsVerifier.forClass(EnvironmentConfiguration.class).withNonnullFields("environment").verify();
+    }
+}
